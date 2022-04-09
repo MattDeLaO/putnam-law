9d3f4b2a6ebb6cc76599b27911fd335c9073a97a
MattDeLaO <metasploitmatt@gmail.com>
2022-04-09 18:12:07 -0500

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,8 +24,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#212430`,
+        theme_color: `#212430`,
         display: `minimal-ui`,
         icon: `src/images/lawIcon.png`, // This path is relative to the root of the site.
       },
